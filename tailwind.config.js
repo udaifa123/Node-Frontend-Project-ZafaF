ef44b29840edf07eee625e0edaa0afb40ca84b14
udaifa123 <udaifakk509@gmail.com>
2026-01-10 21:46:35 +0530

    Node-Frontend-Project-ZafaF

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-module.exports = {
-  purge: [],
-  darkMode: false, 
+// module.exports = {
+//   purge: [],
+//   darkMode: false, 
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-export default {
-  plugins: [tailwindcss()],
-};
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//      tailwindcss(),
+//     react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
